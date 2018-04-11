c405cd32e9bc5eb733da04833b4da807e2cc325d
xyz607xx <xyz607xx@gmail.com>
2018-04-11 18:04:47 +0800

    sql

---
+++ esports.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018-04-11 12:04:37
+-- 伺服器版本: 10.1.31-MariaDB
+-- PHP 版本： 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `esports`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `signup`
+--
+
+CREATE TABLE `signup` (
+  `s_id` int(10) NOT NULL,
+  `s_department` varchar(10) NOT NULL,
+  `s_class` varchar(10) NOT NULL,
+  `s_teamName` varchar(20) NOT NULL,
+  `s_leaderName` varchar(10) NOT NULL,
+  `s_leaderId` varchar(100) NOT NULL,
+  `s_member` text NOT NULL,
+  `s_lineId` varchar(100) NOT NULL,
+  `s_phone` varchar(100) NOT NULL,
+  `s_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `s_win` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`s_id`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `s_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
