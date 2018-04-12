8a26518f2b2b8a99f43152aec6352cf477fd3459
xyz607xx <xyz607xx@gmail.com>
2018-04-12 15:40:56 +0800

    管理員可不用輸入ID修改或刪除報名資訊

--- verificationId.php
+++ verificationId.php
@@ -1,5 +1,6 @@
 <?php
     include_once('db.php');
+    session_start();
     $id = $_POST['index'];
     $user = $_POST['userInput'];
     $uri = $_POST['uri'];
@@ -9,9 +10,9 @@
     $query->execute();
     $result = $query->fetch(PDO::FETCH_ASSOC);
     $arr = [];
-    if($result['s_lineId'] == $user){
+    if($result['s_lineId'] == $user || (isset($_SESSION['login']) && $_SESSION['login']=='admin')){
         $arr['status'] = 'OK';
-        $arr['url'] = $uri.'.php?id='.$id.'&code='.md5($user);
+        $arr['url'] = $uri.'.php?id='.$id.'&code='.md5($result['s_lineId']);
     }else
         $arr['status'] = 'Err';
     echo json_encode($arr);
